e4c0ef2d9060bcdfead4b9791a93fcb9a3c5f14e
ArrreMayuGaming <mayureshshitole@gmail.com>
2021-10-16 17:34:46 +0530

    footer error resolved

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -41,7 +41,7 @@ const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              classNameName="w-5 h-5"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -55,7 +55,7 @@ const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              classNameName="w-5 h-5"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -69,7 +69,7 @@ const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="0"
-              classNameName="w-5 h-5"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <path
